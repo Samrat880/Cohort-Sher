a8cdddc6fd5af3311a177281b84ac60dd98ec8cd
Samrat880 <90104862+Samrat880@users.noreply.github.com>
2026-01-06 19:22:52 +0530

    Day46 assignment project 3 completed

--- JSLearning/Day46 Assignment/3rd/script.js
+++ JSLearning/Day46 Assignment/3rd/script.js
@@ -1,16 +1,71 @@
-let elem = document.querySelectorAll('.elem')
+// let elem = document.querySelectorAll('.elem')
 
-elem.forEach((e) => {
-    let img = e.querySelector('img');
+// elem.forEach((e) => {
+//     let img = e.querySelector('img');
     
-    e.addEventListener('mouseenter', () => {
-        img.style.opacity = '1'
-    });
-    e.addEventListener('mouseleave', () => {
-        img.style.opacity = "0";
-    });
+//     e.addEventListener('mouseenter', () => {
+//         img.style.opacity = '1'
+//     });
+//     e.addEventListener('mouseleave', () => {
+//         img.style.opacity = "0";
+//     });
 
-    e.addEventListener('mousemove', function (dets) {
-        img.style.left = dets.x + 'px'
-    })
-})
+//     e.addEventListener('mousemove', function (dets) {
+//         img.style.left = dets.x + 'px'
+//         img.style.top = dets.y + 'px'
+//     })
+// })
+
+// let elem = document.querySelectorAll(".elem");
+
+// elem.forEach((e) => {
+//   let img = e.querySelector("img");
+
+//   e.addEventListener("mouseenter", () => {
+//     img.style.opacity = "1";
+//   });
+//   e.addEventListener("mouseleave", () => {
+//     img.style.opacity = "0";
+//   });
+
+//   e.addEventListener("mousemove", function (dets) {
+//     // Get position relative to the .elem container
+//     let rect = e.getBoundingClientRect();
+//     let x = dets.clientX - rect.left;
+//     let y = dets.clientY - rect.top;
+
+//     img.style.left = x + "px";
+//     img.style.top = y + "px";
+//   });
+// });
+
+
+let elems = document.querySelectorAll(".elem");
+
+elems.forEach((elem) => {
+  let img = elem.querySelector("img");
+  let imgSize = 150;
+  let half = imgSize / 2;
+
+  elem.addEventListener("mouseenter", () => {
+    img.style.opacity = "1";
+  });
+
+  elem.addEventListener("mouseleave", () => {
+    img.style.opacity = "0";
+  });
+
+  elem.addEventListener("mousemove", (e) => {
+    let rect = elem.getBoundingClientRect();
+
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    // CLAMP inside boundaries
+    x = Math.max(half, Math.min(x, rect.width - half));
+    y = Math.max(half, Math.min(y, rect.height - half));
+
+    img.style.left = x + "px";
+    img.style.top = y + "px";
+  });
+});
